888c73f1a70521bdffc6d3077bf8731f729fdebd
Rahul Changlani <rahul.changlani@gmail.com>
2017-08-22 14:03:52 +0530

    filter pipe and pure/impure pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
